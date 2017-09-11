49481dcca5bfb02332e9f4893bf026493468ba62
dariosantosbsb <dariobsb@hotmail.com>
2017-09-11 15:36:02 -0300

    [ADD] adicionando as entities e as enums

---
+++ src/main/java/com/kazale/pontointeligente/api/enums/tipoEnum.java
@@ -0,0 +1,10 @@
+package com.kazale.pontointeligente.api.enums;
+
+public enum tipoEnum {
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	ININIO_ALMOCO,
+	TERMINO_ALMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA
+}
